13329171c98772a50877123855a1e91dee50e6b3
oswonoswin <atori@tlen.pl>
2017-05-16 17:52:01 +0200

    added sharable docs field

--- Edu2/server/main.js
+++ Edu2/server/main.js
@@ -1,7 +1,14 @@
 import { Meteor } from 'meteor/meteor';
 import '../imports/file-collection.js';
 import '../imports/modules.js';
+import '../imports/tasks.js';
+import {Documents} from '../imports/documents.js';
 
-Meteor.startup(() => {
-  // code to run on server at startup
+Meteor.startup(function() {
+    if (Documents.find().count() === 0) {
+        return Documents.insert({
+            name: "Sample doc",
+            text: "Write here..."
+        });
+    }
 });
