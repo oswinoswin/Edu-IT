d51f0124cb19b821ff50ecc6d0f4931ac7bf6ee9
oswinoswin <atori@tlen.pl>
2017-11-14 12:33:09 +0200

    add module 3 content

--- client/main.js
+++ client/main.js
@@ -45,12 +45,6 @@ Template.body.helpers({
 });
 
 
-Template.leftMenu.helpers({
-    modules() {
-        return Modules.find({}, {sort: {number: 1}});
-    },
-});
-
 
 
 
@@ -169,4 +163,30 @@ Template.moduleContent.helpers({
         console.log("Should be displayed: " + toDisp);
         return toDisp;
     },
+});
+
+
+
+Template.leftMenu.helpers({
+    /*modules() {
+        return Modules.find({}, {sort: {number: 1}});
+    },*/
+
+
+    modules: [
+
+        { number: '1' },
+        { number: '2' },
+        { number: '3' },
+        { number: '4' },
+        { number: '5' },
+        { number: '6' },
+        { number: '7' },
+        { number: '8' },
+        { number: '9' },
+        { number: '10' },
+        { number: '11' },
+        { number: '12' },
+
+    ],
 });
